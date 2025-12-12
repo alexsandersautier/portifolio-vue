c6f0612c16c5ff3c3f9390446eccb9808a6e5bda
Alexander <alexsanderesramos@gmail.com>
2025-12-12 17:32:15 -0300

    refactor: add data

---
+++ src/data/projects.js
@@ -0,0 +1,121 @@
+export const allProjects = [
+    {
+        title: 'Belle Fête',
+        description: 'message.belleFete',
+        imageUrl: '/belle-fete.webp',
+        category: 'message.freelance',
+        isDevelopment: false,
+        tecnologies: [
+            {
+                name: 'Vue JS',
+                type: 'badge-success',
+                img: '/icons/vue.png',
+            },
+            {
+                name: 'DaisyUI',
+                type: 'badge-warning',
+                img: '/icons/daisyUI.png',
+            },
+            {
+                name: 'Supabase',
+                type: 'badge-success',
+                img: '/icons/supabase.png',
+            },
+        ],
+    },
+    {
+        title: 'Gkids Aba',
+        description: 'message.Gkids',
+        imageUrl: '/gkids.png',
+        projectUrl: 'https://gkidsaba.com.br/admin/login',
+        category: 'message.freelance',
+        isDevelopment: false,
+        tecnologies: [
+            {
+                name: 'Laravel',
+                type: 'badge-error',
+                img: '/icons/laravel.png',
+            },
+        ],
+    },
+    {
+        title: 'Academia Space Fit Ourilândia',
+        description: 'message.SpaceFit',
+        imageUrl: '/logo-spacefit.jpg',
+        projectUrl: 'https://www.academiaspacefit.com.br/',
+        category: 'message.freelance',
+        isDevelopment: false,
+        tecnologies: [
+            {
+                name: 'Vue JS',
+                type: 'badge-success',
+                img: '/icons/vue.png',
+            },
+            {
+                name: 'DaisyUI',
+                type: 'badge-warning',
+                img: '/icons/daisyUI.png',
+            },
+        ],
+    },
+    {
+        title: 'MVP Rick and Morty',
+        description: 'message.rickAndMorty',
+        imageUrl: '/rick-and-morty.png',
+        projectUrl: 'https://devalexsander-rickandmorty.netlify.app/',
+        category: 'message.course',
+        isDevelopment: false,
+        tecnologies: [
+            {
+                name: 'Vue JS',
+                type: 'badge-success',
+                img: '/icons/vue.png',
+            },
+            {
+                name: 'DaisyUI',
+                type: 'badge-warning',
+                img: '/icons/daisyUI.png',
+            },
+        ],
+    },
+    {
+        title: 'Promobile',
+        description: 'message.promobile',
+        imageUrl: '/promobile.png',
+        projectUrl: 'https://play.google.com/store/apps/details?id=net.proxsis.promobile&hl=pt_BR',
+        category: 'message.wordExperience',
+        isDevelopment: false,
+        tecnologies: [
+            {
+                name: 'Ionic 3',
+                type: 'badge-success',
+                img: '/icons/ionic.png',
+            },
+            {
+                name: 'Angular 5',
+                type: 'badge-warning',
+                img: '/icons/angular.png',
+            },
+        ],
+    },
+    {
+        title: 'Contrestoque',
+        description: 'message.contrestoque',
+        imageUrl: '/sge-vue.png',
+        projectUrl: 'https://sge-vue.vercel.app/',
+        category: 'message.course',
+        isDevelopment: true,
+        tecnologies: [
+            {
+                name: 'Vue JS',
+                type: 'badge-success',
+                img: '/icons/vue.png',
+            },
+            {
+                name: 'Vuetify',
+                type: 'badge-warning',
+                img: '/icons/vuetify.svg',
+            },
+        ],
+    },
+]
